5e5cd1be4992ab3ee2fcd947b6c9abbbf1dea983
phakphoom <chphakphoom@gmail.com>
2019-01-04 14:19:12 +0700

    Initial commit

---
+++ src/test/java/test/simple/NewTest.java
@@ -0,0 +1,57 @@
+package test.simple;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class NewTest {
+    private WebDriver driver;
+    @Test
+    public void testEasy() throws InterruptedException {
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless");
+
+        System.setProperty("webdriver.chrome.driver", "/home/phakphoom/chromedriver");
+        driver  = new ChromeDriver(options);
+
+        this.driver.manage().window().maximize();
+        this.driver.manage().deleteAllCookies();
+        this.driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+        this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
